49e83b6f8e7a94caf29d3d159ef690e014ab556f
harshit2001sharma@gmail.com <44293679+Harshit123Sharma@users.noreply.github.com>
2021-07-28 14:49:54 +0530

    Updated backend

--- server/index.js
+++ server/index.js
@@ -10,21 +10,19 @@ const port = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
-
 const uri = process.env.ATLAS_URI;
 mongoose.connect(uri, { useNewUrlParser: true, useCreateIndex: true });
 const connection = mongoose.connection;
 connection.once("open", () => {
-    console.log("DATABASE Connection Successful!!!");
+  console.log("MongoDB database connection established successfully");
 });
 
 const exercisesRouter = require("./routes/exercises");
-const userRouter = require("./routes/users");
+const usersRouter = require("./routes/users");
 
-app.use("./exercises", exercisesRouter);
-app.use("./users", userRouter);
-
+app.use("/exercises", exercisesRouter);
+app.use("/users", usersRouter);
 
 app.listen(port, () => {
-    console.log(`Server is running on port: ${port}`);
+  console.log(`Server is running on port: ${port}`);
 });
